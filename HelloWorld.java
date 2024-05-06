56d13eddb9dffe5e94b8deffbfb0ae4c1385a634
Onderito <ulas.onder@outlook.fr>
2024-05-06 22:10:07 +0200

    ok

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ class ClassName {
     public static void main(String[] args) {
         // afficher Hello World! dans le terminal
         System.out.println("Hello World!");
+        int number = 4;
+        
     }
+
 }
